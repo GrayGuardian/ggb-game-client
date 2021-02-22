a143fb6909d5571d7592448a847e4dd7029ba86c
GrayGuardian <zjqtsw@163.com>
2021-02-22 22:44:13 +0800

    修复Comp Btn问题；

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,7 +4,6 @@ public class Main : MonoBehaviour
 {
     void Awake()
     {
-        //gameObject.AddComponent<UnityEngine.UI.Image>().color = 
         MonoSingleton.Instance.MonoGo.AddComponent<LuaClient>();
     }
 }
